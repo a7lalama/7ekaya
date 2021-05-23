561c8c86b239e1111b011f500843986c18b10e8b
ahmedto <ahmed.to2004@gmail.com>
2021-05-23 18:16:45 +0200

    Update a7la7ekaya21k.js

--- a7lalama/js/a7la7ekaya21k.js
+++ a7lalama/js/a7la7ekaya21k.js
@@ -1222,9 +1222,8 @@
     
     var F = S.Deferred();
 function B() {
-        E.removeEventListener("DOMContentLoaded", B),  S.ready()
+        E.removeEventListener("DOMContentLoaded", B), C.removeEventListener("load", B), S.ready()
     }
-    
     S.fn.ready = function(e) {
         return F.then(e)["catch"](function(e) {
             S.readyException(e)
