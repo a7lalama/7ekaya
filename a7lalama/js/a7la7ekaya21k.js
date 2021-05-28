dcc951e11c8a1c33f78a70d3c5d6d55e1c1cbeaf
ahmedto <ahmed.to2004@gmail.com>
2021-05-28 21:54:30 +0200

    Update a7la7ekaya21k.js

--- a7lalama/js/a7la7ekaya21k.js
+++ a7lalama/js/a7la7ekaya21k.js
@@ -1326,6 +1326,29 @@
             }, null, e, 1 < arguments.length, null, !0)
         },
        
+    }), S.extend({
+        queue: function (e, t, n) {
+            var r;
+            if (e) return t = (t || "fx") + "queue", r = Y.get(e, t), n && (!r || Array.isArray(n) ? r = Y.access(e, t, S.makeArray(n)) : r.push(n)), r || []
+        },
+        dequeue: function (e, t) {
+            t = t || "fx";
+            var n = S.queue(e, t),
+                r = n.length,
+                i = n.shift(),
+                o = S._queueHooks(e, t);
+            "inprogress" === i && (i = n.shift(), r--), i && ("fx" === t && n.unshift("inprogress"), delete o.stop, i.call(e, function () {
+                S.dequeue(e, t)
+            }, o)), !r && o && o.empty.fire()
+        },
+        _queueHooks: function (e, t) {
+            var n = t + "queueHooks";
+            return Y.get(e, n) || Y.access(e, n, {
+                empty: S.Callbacks("once memory").add(function () {
+                    Y.remove(e, [t + "queue", n])
+                })
+            })
+        }
     }), S.fn.extend({
         queue: function (t, n) {
             var e = 2;
