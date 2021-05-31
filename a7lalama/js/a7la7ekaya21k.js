7135f483abee7cbd0261bb615e2ea88f784565d1
ahmedto <ahmed.to2004@gmail.com>
2021-06-01 01:33:47 +0200

    Update a7la7ekaya21k.js

--- a7lalama/js/a7la7ekaya21k.js
+++ a7lalama/js/a7la7ekaya21k.js
@@ -310,9 +310,7 @@
                 return !!e(t)
             } catch (e) {
                 return !1
-            } finally {
-                t.parentNode && t.parentNode.removeChild(t), t = null
-            }
+            } 
         }
         
         function fe(e, t) {
