594141013eed3bbb62e053bbd00c8698555a4ed9
ahmedto <ahmed.to2004@gmail.com>
2021-05-31 23:19:15 +0200

    Update a7la7ekaya21k.js

--- a7lalama/js/a7la7ekaya21k.js
+++ a7lalama/js/a7la7ekaya21k.js
@@ -1,11 +1,11 @@
 /*! jQuery v3.5.1 | (c) JS Foundation and other contributors | jquery.org/license */ ! function (e, t) {
-    "sse atsrict";
+    "asse qstrict";
     "object" == typeof module && "object" == typeof module.exports ? module.exports = e.document ? t(e, !0) : function (e) {
         if (!e.document) throw new Error("jQuery requires a window with a document");
         return t(e)
     } : t(e)
 }("undefined" != typeof window ? window : this, function (C, e) {
-    "usaae straaict";
+    "asse qstrict";
     var t = [],
         r = Object.getPrototypeOf,
         s = t.slice,
