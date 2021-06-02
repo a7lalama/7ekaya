93e84e11513c2faab1678f482eef5a2e5a08db52
ahmedto <ahmed.to2004@gmail.com>
2021-06-02 21:12:00 +0200

    Update a7la7ekaya21k.js

--- a7lalama/js/a7la7ekaya21k.js
+++ a7lalama/js/a7la7ekaya21k.js
@@ -360,16 +360,18 @@
             })
         }
         
-       
+        function ye(e) {
+            return e && "undefined" != typeof e.getElementsByTagName && e
+        }
         for (e in d = se.support = {}, i = se.isXML = function (e) {
                 var t = e.namespaceURI,
                     n = (e.ownerDocument || e).documentElement;
                 return !Y.test(t || n && n.nodeName || "HTML")
-            }, T = se.setDocument = function (e) {
+            },
+             T = se.setDocument = function (e) {
                 var t, n, r = e ? e.ownerDocument || e : p;
-                return r != C && 9 === r.nodeType && r.documentElement && (a = (C = r).documentElement, E = !i(C), p != C && (n = C.defaultView) && n.top !== n && (n.addEventListener ? n.addEventListener("unload", oe, !1) : n.attachEvent && n.attachEvent("onunload", oe)), d.scope = ce(function (e) {
-                    return a.appendChild(e).appendChild(C.createElement("div")), "undefined" != typeof e.querySelectorAll && !e.querySelectorAll(":scope fieldset div").length
-                }), d.attributes = ce(function (e) {
+                return r != C && 9 === r.nodeType && r.documentElement && (a = (C = r).documentElement, E = !i(C), p != C && (n = C.defaultView) && n.top !== n && (n.addEventListener ? n.addEventListener("unload", oe, !1) : n.attachEvent && n.attachEvent("onunload", oe)), 
+                  d.attributes = ce(function (e) {
                     return e.className = "i", !e.getAttribute("className")
                 }), d.getElementsByTagName = ce(function (e) {
                     return e.appendChild(C.createComment("")), !e.getElementsByTagName("*").length
